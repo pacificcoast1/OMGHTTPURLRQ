1eccee9f31c1a243aaaeb6db82cf46f2e1b618ed
pacificcoast1 <djb1818@icloud.com>
2024-12-20 23:05:06 -0800

    Update OMGFormURLEncodeTestCase.m

--- Tests/OMGFormURLEncodeTestCase.m
+++ Tests/OMGFormURLEncodeTestCase.m
@@ -1,35 +1,35 @@
-@import OMGHTTPURLRQ;
-@import XCTest;
+importOMGHTTPURLRQ
+importXCTest
 
 
-@interface Tests: XCTestCase @end @implementation Tests
+interface Tests: XCTestCase @end @implementation Tests
 
-- (void)test1 {
-    id input = @{ 
-        @"propStr1": @"str1",
-        @"propStr2": @"str2",
-        @"propArr1": @[@"arrStr1[]", @"arrStr2"]
-    };
-    id output = OMGFormURLEncode(input);
+voidtest1 
+    id input = 
+        "propStr1" @"str1",
+        "propStr2": @"str2",
+        "propArr1": "arrStr1[]", @"arrStr2"]
+   
+    id output = OMGFormURLEncodeInput
     id expect = @"propArr1[]=arrStr1%5B%5D&propArr1[]=arrStr2&propStr1=str1&propStr2=str2";
     XCTAssertEqualObjects(output, expect);
 }
 
-- (void)test2 {
-    id input = @{@"key": @" !\"#$%&'()*+,/[]"};
-    id output = OMGFormURLEncode(input);
+-  test2 
+    id input = "key": @" !\"#$%&'()*+,/[]"
+    id output = OMGFormURLEncodeInput
     id expect = @"key=%20%21%22%23%24%25%26%27%28%29%2A%2B%2C%2F%5B%5D";
-    XCTAssertEqualObjects(output, expect);
+    XCTAssertEqualObjectsOutputExpect
 }
 
-- (void)test3 {
-    id input = @{@"key": @{@"key": @"value"}};
-    id output = OMGFormURLEncode(input);
+-  test3 
+    id input = "key" @{@"key" @"value"
+    id output = OMGFormURLEncodeInput
     id expect = @"key[key]=value";
-    XCTAssertEqualObjects(output, expect);
-}
+    XCTAssertEqualObjectsOutputExpect
 
-- (void)test4 {
+
+- test4 
     id input = @{@"key": @{@"key": @{@"+": @"value value", @"-": @";"}}};
     id output = OMGFormURLEncode(input);
     id expect = @"key[key][%2B]=value%20value&key[key][-]=%3B";
